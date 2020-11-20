1155ce4dab8f683b68f36c0b239d5746e29df76b
Tumi <oluwatumininuojo25@gmail.com>
2020-11-20 22:46:35 +0100

    A simple ToDO app

--- app/src/main/java/com/tecx/todo/Const.kt
+++ app/src/main/java/com/tecx/todo/Const.kt
@@ -12,6 +12,3 @@ const val TABLE_TODO_ITEM = "ToDoListItem"
 const val COL_TODO_ID = "toDoId"
 const val COL_ITEM_NAME = "itemName"
 const val COL_IS_COMPLETED = "isCompleted"
-
-const val INTENT_TODO_ID = "TodoId"
-const val INTENT_TODO_NAME = "TodoName"
