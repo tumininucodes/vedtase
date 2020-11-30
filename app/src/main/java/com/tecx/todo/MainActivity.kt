1d658cb5533971e133381667566931f61f6c5845
Tumi <oluwatumininuojo25@gmail.com>
2020-11-30 08:29:21 +0100

    A simple ToDO app

--- app/src/main/java/com/tecx/todo/MainActivity.kt
+++ app/src/main/java/com/tecx/todo/MainActivity.kt
@@ -2,11 +2,7 @@ package com.tecx.todo
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
-import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,22 +10,8 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ic_logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_in))
-
-
-        Handler().postDelayed({
-
-            ic_logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_out))
-
-            Handler().postDelayed({
-
-                ic_logo.visibility = View.GONE
-                startActivity(Intent(this, TodoListActivity::class.java))
-                finish()
-
-            }, 500)
-
-        }, 1500)
+        startActivity(Intent(this, TodoListActivity::class.java))
+        finish()
 
     }
 }
